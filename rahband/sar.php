60c5c16ff0229db1f59e2f8a77babe0d5660f4c5
astarawork <nimaseyfinejad@gmail.com>
2025-07-31 00:46:03 +0330

    آپلود خودکار: rahband/sar.php - 2025-07-31 00:46:03

---
+++ rahband/sar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang = "fa">
+    <meta charset="utf-8" />
+
+    <head>
+   <link rel="stylesheet" href="/jalalidatepicker/dist/jalalidatepicker.min.css">
+<script type="text/javascript" src="/jalalidatepicker/dist/jalalidatepicker.min.js"></script>
+    
+
+        <script src="/files/jquery22slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="/files/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="/files/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="/files/bootstrap.min.css" rel="stylesheet">
+  <script src="/files/bootstrap.bundle.min.js"></script>
+     
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	
+
