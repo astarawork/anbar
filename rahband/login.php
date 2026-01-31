c86dee86410ed65ad80db20f50e5c52432e6300b
astarawork <nimaseyfinejad@gmail.com>
2026-01-31 15:00:22 +0330

    آپلود خودکار: rahband/login.php - 2026-01-31 15:00

--- rahband/login.php
+++ rahband/login.php
@@ -62,9 +62,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ورود به سیستم</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-rtl@5.3.0/dist/css/bootstrap-rtl.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link href="https://anbar20.ir/files/bootstrap.min.css" rel="stylesheet">
+    <link href="https://anbar20.ir/files/bootstrap-rtl.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://anbar20.ir/files/all.min.css">
     <style>
         body {
             background-color: #f8f9fa;
